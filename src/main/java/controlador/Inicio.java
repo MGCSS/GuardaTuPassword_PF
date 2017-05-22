f02cbcbcde2f5ac76833490086bc46d4c01e22cf
Juan Antonio Fernandez <juaniyofm@gmail.com>
2017-05-22 18:58:41 +0200

    Finalizada clase ManejaUsuario, Versión 1

--- src/main/java/controlador/Inicio.java
+++ src/main/java/controlador/Inicio.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package controlador;
 
+import java.util.List;
 import modelo.*;
 //import static java.lang.Thread.sleep;
 //import java.sql.Time;
@@ -20,16 +20,27 @@ public class Inicio {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws InterruptedException {
-        ManejaUsuario mu = new ManejaUsuario(); 
-        Usuarios usuario = new Usuarios("pepe", "pepe"); 
-        mu.guardaUsuario(usuario); 
- 
-        Cuentas cuenta = new Cuentas(usuario, "cuenta1", "cuenta1pass"); 
-        mu.guardaCuenta(cuenta); 
- 
-        Usuarios usuario1 = new Usuarios(); 
-        usuario = mu.obtenUsuario(1); 
-        System.out.println(usuario.getNombreInicio());
+        ManejaUsuario mu = new ManejaUsuario();
+        //Usuarios usuario = new Usuarios("pepe", "pepe"); 
+        //mu.guardaUsuario(usuario); 
+
+        //Cuentas cuenta = new Cuentas(usuario, "cuenta1", "cuenta1pass"); 
+        //mu.guardaCuenta(cuenta); 
+        Usuarios usuario1 = new Usuarios();
+        usuario1 = mu.obtenUsuario("juanfran");
+        System.out.println("ID: " + usuario1.getIdInicio() + ", NOMBRE: " + usuario1.getNombreInicio() + ", PASSWORD: " + usuario1.getPassInicio());
+
+        //mu.eliminaUsuario(usuario1);
+        //usuario1.setPassInicio("newpass");
+        //mu.modificarUsuario(usuario1);
+        /*List<Object[]> r = mu.obtenCuentas(usuario1.getIdInicio());
+        for (int i = 0; i < r.size(); i++) {
+            System.out.println(r.get(i)[2]);
+        }*/
+        
+        Cuentas c = new Cuentas();
+        System.out.println(mu.obtenCuenta("githubjuanfran").getNombre());
+
     }
-    
+
 }
--- src/main/java/controlador/ManejaUsuario.java
+++ src/main/java/controlador/ManejaUsuario.java
@@ -3,36 +3,34 @@ package controlador;
 import modelo.*;
 import org.hibernate.*;
 import java.util.List;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.LogicalExpression;
 import org.hibernate.criterion.Restrictions;
+
 /**
  *
  * @author usuario
  */
 public class ManejaUsuario {
+
     private Session sesion;
     private Transaction tran;
-    public ManejaUsuario()
-    {
-        
+
+    public ManejaUsuario() {
+
     }
-    public void inicioOperacion() throws HibernateException
-    {
-        sesion=HibernateUtil.getSessionFactory().openSession();
-        tran=sesion.beginTransaction();
+
+    public void inicioOperacion() throws HibernateException {
+        sesion = HibernateUtil.getSessionFactory().openSession();
+        tran = sesion.beginTransaction();
         System.out.println("Operacion iniciada.");
     }
-    public void guardaExperto(Usuarios usuario){
-        try {
-            inicioOperacion();
-            sesion.save(usuario);
-            System.out.println("usuario insertado correctamente");
-        } catch (HibernateException he) {
-            throw he;
-        } finally {
-            finalizaOperacion();
-        }
-    }    
-    
+
+    /**
+     * Almacena un nuevo usuario en la aplición.
+     *
+     * @param usuario
+     */
     public void guardaUsuario(Usuarios usuario) {
         try {
             inicioOperacion();
@@ -45,6 +43,11 @@ public class ManejaUsuario {
         }
     }
 
+    /**
+     * Almacena en la BD una cuenta de usuario.
+     *
+     * @param cuenta
+     */
     public void guardaCuenta(Cuentas cuenta) {
         try {
             inicioOperacion();
@@ -57,6 +60,12 @@ public class ManejaUsuario {
         }
     }
 
+    /**
+     * Obtiene un usuario en concreto.
+     *
+     * @param id
+     * @return
+     */
     public Usuarios obtenUsuario(Integer id) {
         try {
             inicioOperacion();
@@ -68,24 +77,122 @@ public class ManejaUsuario {
         }
     }
 
-    public void obtenCuentas(Integer idUsuario) {
+    /**
+     * Obtiene un usuario en concreto.
+     *
+     * @param nombre
+     * @return
+     */
+    public Usuarios obtenUsuario(String nombre) {
         try {
             inicioOperacion();
-            String queryString = "SELECT * FROM cuentas c WHERE c.usuario='" + idUsuario + "'";
-            Query query = sesion.createSQLQuery(queryString);
-            List<Object[]> cuentas = query.list();
-            for (int i = 0; i < cuentas.size(); i++) {
-                System.out.println("Cuenta: " + cuentas.get(i)[2]);
-            }
+            Criteria cr = sesion.createCriteria(Usuarios.class);
+            cr.add(Restrictions.eq("nombreInicio", nombre));
+            List results = cr.list();
+            Usuarios u = new Usuarios();
+            u = (Usuarios) results.get(0);
+            return u;
         } catch (HibernateException he) {
             throw he;
         } finally {
             finalizaOperacion();
         }
     }
-    public void finalizaOperacion(){
+
+    /**
+     * Obtiene todas las cuentas almacenadas de un usuario.
+     *
+     * @param idUsuario
+     */
+    public List<Object[]> obtenCuentas(Integer idUsuario) {
+        try {
+            inicioOperacion();
+            String queryString = "SELECT * FROM cuentas c WHERE c.usuario='" + idUsuario + "'";
+            Query query = sesion.createSQLQuery(queryString);
+            List<Object[]> cuentas = query.list();
+            return cuentas;
+        } catch (HibernateException he) {
+            throw he;
+        } finally {
+            finalizaOperacion();
+        }
+    }
+
+    /**
+     * Obtiene una cuenta en concreto almacenada de un usuario.
+     *
+     * @param nombre
+     */
+    public Cuentas obtenCuenta(String nombre) {
+        try {
+            inicioOperacion();
+            Criteria cr = sesion.createCriteria(Cuentas.class);
+            cr.add(Restrictions.eq("nombre", nombre));
+            List results = cr.list();
+            Cuentas c = new Cuentas();
+            c = (Cuentas) results.get(0);
+            return c;
+        } catch (HibernateException he) {
+            throw he;
+        } finally {
+            finalizaOperacion();
+        }
+    }
+
+    /**
+     * Elimina un usuario de la BD.
+     *
+     * @param usuario
+     */
+    public void eliminaUsuario(Usuarios usuario) {
+        try {
+            inicioOperacion();
+            sesion.delete(usuario);
+            System.out.println("usuario eliminado correctamente");
+        } catch (HibernateException he) {
+            throw he;
+        } finally {
+            finalizaOperacion();
+        }
+    }
+
+    /**
+     * Elimina una cuenta de un usuario en concreto de la BD.
+     *
+     * @param cuenta
+     */
+    public void eliminaCuenta(Cuentas cuenta) {
+        try {
+            inicioOperacion();
+            sesion.delete(cuenta);
+            System.out.println("Cuenta eliminada correctamente");
+        } catch (HibernateException he) {
+            throw he;
+        } finally {
+            finalizaOperacion();
+        }
+    }
+
+    /**
+     * Modificar un usuario.
+     *
+     * @param usuario
+     */
+    public void modificarUsuario(Usuarios usuario) {
+        try {
+            inicioOperacion();
+            sesion.update(usuario);
+            System.out.println("Usuario actualizado correctamente");
+        } catch (HibernateException he) {
+            throw he;
+        } finally {
+            finalizaOperacion();
+        }
+    }
+
+    public void finalizaOperacion() {
         tran.commit();
-        sesion.close();    
+        sesion.close();
         System.out.println("Operacion finalizada.");
     }
 }
