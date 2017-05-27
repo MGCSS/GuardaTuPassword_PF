2b75a62edc6c6e7fa35a33f9d10371c89535f726
Juan Antonio Fernandez <juaniyofm@gmail.com>
2017-05-27 21:11:20 +0200

    Modificada la forma en que se encripta las passwords en el Script BD

--- BD_GuardaTuPassword.sql
+++ BD_GuardaTuPassword.sql
@@ -18,32 +18,32 @@ CONSTRAINT FKusuario FOREIGN KEY (usuario) REFERENCES USUARIOS (idInicio) on del
 
 /* Cuentas de conexion a la aplicacion */
 
-INSERT INTO USUARIOS VALUES (1,'juan','passjuan');
-INSERT INTO USUARIOS VALUES (2,'juanfran','passjuanfran');
-INSERT INTO USUARIOS VALUES (3,'agustin','passagustin');
-INSERT INTO USUARIOS VALUES (4,'carlos','passcarlos');
+INSERT INTO USUARIOS VALUES (1,'juan', AES_ENCRYPT ('passjuan','pass'));
+INSERT INTO USUARIOS VALUES (2,'juanfran', AES_ENCRYPT ('passjuanfran','pass'));
+INSERT INTO USUARIOS VALUES (3,'agustin', AES_ENCRYPT ('passagustin','pass'));
+INSERT INTO USUARIOS VALUES (4,'carlos', AES_ENCRYPT ('passcarlos','pass'));
 
 /* Listado de Cuentas de cada usuario */
 
-INSERT INTO CUENTAS VALUES (1,1,'correoUHUjuan','uhujuan','Cuenta de correo de la UHU');
-INSERT INTO CUENTAS VALUES (2,1,'githubjuan','gitjuan','Cuenta de gitHub');
-INSERT INTO CUENTAS VALUES (3,1,'gmailjuan','gmailjuan','Cuenta de correo de Gmail');
-INSERT INTO CUENTAS VALUES (4,1,'hotmailjuan','hotjuan','Cuenta de correo de hotmail');
+INSERT INTO CUENTAS VALUES (1,1,'correoUHUjuan', AES_ENCRYPT ('uhujuan','pass'),'Cuenta de correo de la UHU');
+INSERT INTO CUENTAS VALUES (2,1,'githubjuan', AES_ENCRYPT ('gitjuan','pass'),'Cuenta de gitHub');
+INSERT INTO CUENTAS VALUES (3,1,'gmailjuan', AES_ENCRYPT ('gmailjuan','pass'),'Cuenta de correo de Gmail');
+INSERT INTO CUENTAS VALUES (4,1,'hotmailjuan', AES_ENCRYPT ('hotjuan','pass'),'Cuenta de correo de hotmail');
 
 
-INSERT INTO CUENTAS VALUES (5,2,'githubjuanfran','gitjuanfran','Cuenta de gitHub');
-INSERT INTO CUENTAS VALUES (6,2,'gmailjuanfran','gmailjuanfran','Cuenta de correo de Gmail');
-INSERT INTO CUENTAS VALUES (7,2,'hotmailjuanfran','hotjuanfran','Cuenta de correo de hotmail');
-INSERT INTO CUENTAS VALUES (8,2,'ebayjuanfran','ebyjuanfran','Cuenta de Ebay');
-INSERT INTO CUENTAS VALUES (9,2,'dropboxjuanfran','dropjuanfran','Cuenta de Dropbox');
+INSERT INTO CUENTAS VALUES (5,2,'githubjuanfran', AES_ENCRYPT ('gitjuanfran','pass'),'Cuenta de gitHub');
+INSERT INTO CUENTAS VALUES (6,2,'gmailjuanfran', AES_ENCRYPT ('gmailjuanfran','pass'),'Cuenta de correo de Gmail');
+INSERT INTO CUENTAS VALUES (7,2,'hotmailjuanfran', AES_ENCRYPT ('hotjuanfran','pass'),'Cuenta de correo de hotmail');
+INSERT INTO CUENTAS VALUES (8,2,'ebayjuanfran', AES_ENCRYPT ('ebyjuanfran','pass'),'Cuenta de Ebay');
+INSERT INTO CUENTAS VALUES (9,2,'dropboxjuanfran', AES_ENCRYPT ('dropjuanfran','pass'),'Cuenta de Dropbox');
 
-INSERT INTO CUENTAS VALUES (10,3,'githubagustin','gitagustin','Cuenta de GitHub');
-INSERT INTO CUENTAS VALUES (11,3,'gmailagustin','gmailagustin','Cuenta de correo de Gmail');
-INSERT INTO CUENTAS VALUES (12,3,'hotmailagustin','hotagustin','Cuenta de correo de hotmail');
-INSERT INTO CUENTAS VALUES (13,3,'stackoveragustin','stackagustin','Cuenta de Stackoverflow');
+INSERT INTO CUENTAS VALUES (10,3,'githubagustin', AES_ENCRYPT ('gitagustin','pass'),'Cuenta de GitHub');
+INSERT INTO CUENTAS VALUES (11,3,'gmailagustin', AES_ENCRYPT ('gmailagustin','pass'),'Cuenta de correo de Gmail');
+INSERT INTO CUENTAS VALUES (12,3,'hotmailagustin', AES_ENCRYPT ('hotagustin','pass'),'Cuenta de correo de hotmail');
+INSERT INTO CUENTAS VALUES (13,3,'stackoveragustin', AES_ENCRYPT ('stackagustin','pass'),'Cuenta de Stackoverflow');
 
 
-INSERT INTO CUENTAS VALUES (14,4,'githubcarlos','gitcarlos','Cuenta de gitHub');
-INSERT INTO CUENTAS VALUES (15,4,'gmailcarlos','gmailcarlos','Cuenta de correo de Gmail');
-INSERT INTO CUENTAS VALUES (16,4,'hotmailcarlos','hotcarlos','Cuenta de correo de hotmail');
-INSERT INTO CUENTAS VALUES (17,4,'correoUHUcarlos','uhucarlos','Cuenta de correo de la UHU');
+INSERT INTO CUENTAS VALUES (14,4,'githubcarlos', AES_ENCRYPT ('gitcarlos','pass'),'Cuenta de gitHub');
+INSERT INTO CUENTAS VALUES (15,4,'gmailcarlos', AES_ENCRYPT ('gmailcarlos','pass'),'Cuenta de correo de Gmail');
+INSERT INTO CUENTAS VALUES (16,4,'hotmailcarlos', AES_ENCRYPT ('hotcarlos','pass'),'Cuenta de correo de hotmail');
+INSERT INTO CUENTAS VALUES (17,4,'correoUHUcarlos', AES_ENCRYPT ('uhucarlos','pass'),'Cuenta de correo de la UHU');
